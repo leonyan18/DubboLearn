53f17208baa958f561486eff0cecc346a832197b
yan <33154558+leonyan18@users.noreply.github.com>
2019-04-13 11:57:49 +0800

    dubbo打包为jar 提供服务
    dubbo打包为jar 提供服务

--- DubboTestService/src/main/java/starter/ItemApplication.java
+++ DubboTestService/src/main/java/starter/ItemApplication.java
@@ -1,6 +1,5 @@
 package starter;
 
-import config.DataConfig;
 import config.RootConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ItemApplication {
 
 	public static void main(String[] args) throws Exception {
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(RootConfig.class, DataConfig.class);
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(RootConfig.class);
 		ctx.start();
         // press any key to exit
         System.in.read();
